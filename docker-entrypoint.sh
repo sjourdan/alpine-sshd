ec542716c97aa507fa9b1cd7fe6b9027fa685ef0
Stephane Jourdan <fasten@fastmail.fm>
2016-02-27 15:37:37 +0100

    added permissions enforcement in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-echo "Creating user"
+echo "Creating user ${USER}"
 adduser -D ${USER} && echo "${USER}:${PASSWORD}" | chpasswd
+echo "Fixing permissions for user ${USER}"
+chown -R ${USER}:${USER} /home/${USER}
 exec "$@"
